54f0af3e706d819fe9b92f2eac59a684a4a8853d
Abraham E. Tavarez <abetavarez@icloud.com>
2022-05-14 00:25:40 -0400

    functions pattern

---
+++ CreationalPattern/functions.js
@@ -0,0 +1,17 @@
+// Function as First Class Citizens
+// It means functions can be used as values
+const cal = () => {
+  return 4 * 3;
+};
+
+const result = cal();
+console.log(result);
+
+// Callback Functions
+const printCalc = (callback) => {
+  console.log(callback());
+};
+
+printCalc(cal);
+
+//
