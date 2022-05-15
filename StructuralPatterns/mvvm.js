fb329e14f60b60d6a79335b57d5c0581cce68f9b
Abraham E. Tavarez <abetavarez@icloud.com>
2022-05-15 19:24:09 -0400

    mvvm js pattern

---
+++ StructuralPatterns/mvvm.js
@@ -0,0 +1,5 @@
+// MVVM or Model View View Model is sometimes refer as the MVVC or Model View View Controller, but the both serve the same purpose.
+// M: Model holds the logic or state of the data
+// V: The first View has no data or logic sometimes refer as (dumb component)
+// V: The second View can be a stateless component with only visuals
+// M: The last model can be a stateful component with both logic/state and view.
