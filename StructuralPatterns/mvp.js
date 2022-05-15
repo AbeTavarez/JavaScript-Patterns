4cb10703489f5dd9d193c5119550edafd695dc9a
Abraham E. Tavarez <abetavarez@icloud.com>
2022-05-15 19:24:29 -0400

    mvp js pattern

---
+++ StructuralPatterns/mvp.js
@@ -0,0 +1,2 @@
+// MVP or Model View Presenter Pattern its similar to MVC
+// In MVC the View has access to both model and controllers but in MVP the View doesn't have to access the model, it has to get it from the Presenter and the Presenter acts as the logic and supply the data
