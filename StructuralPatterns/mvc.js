2521043e32e15f4a23439012fa2162c9ab4f5b1f
Abraham E. Tavarez <abetavarez@icloud.com>
2022-05-15 19:23:51 -0400

    mvc js pattern

---
+++ StructuralPatterns/mvc.js
@@ -0,0 +1,5 @@
+// MVC or Model View Controller Pattern help us organize our code in the following way:
+// Model = Data (Define Schemas for the data)
+// View = Visuals (HTML)
+// Controllers = Logic (functions)
+// (In MVC the View has access to both model and controllers)
